66b88b9d2e065b4dbc5c98222eaeae0255d5768e
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-05-04 16:06:18 -0300

    Moving restaurants list to home component

--- src/app/modules/private/components/home/home.component.ts
+++ src/app/modules/private/components/home/home.component.ts
@@ -1,11 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { RestaurantModel } from 'src/app/core/models/restaurant.model';
+import { RestaurantsService } from 'src/app/core/services/restaurants.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  public restaurantsList: RestaurantModel[] = [];
 
-  ngOnInit() {}
+  constructor(
+    private spinnerService: NgxSpinnerService,
+    private restaurantsService: RestaurantsService
+  ) {}
+
+  ngOnInit() {
+    this.getRestaurantsList();
+  }
+
+  /**
+   * @description Recuperar restaurantes cadastrados no sistema
+   *
+   * @author Darllinson Azevedo
+   */
+  getRestaurantsList() {
+    this.spinnerService.show();
+
+    this.restaurantsService.getRestaurants().subscribe(
+      (data) => {
+        this.restaurantsList = data;
+        this.spinnerService.hide();
+      },
+      (err) => {
+        console.log(err);
+        this.spinnerService.hide();
+      }
+    );
+  }
 }
--- src/app/modules/public/components/login/login.component.ts
+++ src/app/modules/public/components/login/login.component.ts
@@ -1,41 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { RestaurantModel } from 'src/app/core/models/restaurant.model';
-import { RestaurantsService } from 'src/app/core/services/restaurants.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit {
-  public restaurantsList: RestaurantModel[] = [];
+  constructor() {}
 
-  constructor(
-    private spinnerService: NgxSpinnerService,
-    private restaurantsService: RestaurantsService
-  ) {}
-
-  ngOnInit() {
-    this.getRestaurantsList();
-  }
-
-  /**
-   * @description Função para teste do servidor (apagar depois)
-   *
-   * @author Darllinson Azevedo
-   */
-  getRestaurantsList() {
-    this.spinnerService.show();
-
-    this.restaurantsService.getRestaurants().subscribe(
-      (data) => {
-        this.restaurantsList = data;
-        this.spinnerService.hide();
-      },
-      (err) => {
-        console.log(err);
-        this.spinnerService.hide();
-      }
-    );
-  }
+  ngOnInit() {}
 }
