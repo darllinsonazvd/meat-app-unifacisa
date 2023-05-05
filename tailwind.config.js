202dba29d0fd7228eea25bc48d445efc3de79c85
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-05-05 16:47:59 -0300

    Making login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./node_modules/tw-elements/dist/js/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      rotate: {
+        360: "360deg",
+      },
+      keyframes: {
+        float: {
+          "0%": { transform: "translatey(0px)" },
+          "40%": { transform: "translatey(-20px)" },
+          "80%": { transform: "translatey(10px)" },
+          "100%": { transform: "translatey(0px)" },
+        },
+      },
+      animation: {
+        float: "float 4s linear infinite",
+      },
+    },
   },
   plugins: [require("tw-elements/dist/plugin.cjs")],
   darkMode: "class",
