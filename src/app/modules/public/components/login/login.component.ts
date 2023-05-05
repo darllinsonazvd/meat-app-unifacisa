a9a4eb40186c95d4291108e948d98c3ee2e35106
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-05-05 15:34:23 -0300

    Setuping tailwind elements lib

--- src/app/modules/public/components/login/login.component.ts
+++ src/app/modules/public/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Input, Ripple, initTE } from 'tw-elements';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    initTE({ Input, Ripple });
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./src/**/*.{html,ts}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class",
 };
