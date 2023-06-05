4846f0d1eb63fc0069d9b36f33f9775ed1c289c9
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-06-05 15:17:43 -0300

    Enable router link active on navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  public homeIsActive: boolean = false;
+  public myOrdersIsActive: boolean = false;
+  public userProfileIsActive: boolean = false;
 
-  ngOnInit() {}
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.homeIsActive = this.router.isActive('/private/home', true);
+    this.userProfileIsActive = this.router.isActive('/private/profile', true);
+  }
 }
