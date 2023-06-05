cb09117b38400d96b1b0677c0d56e3b913cfd5cb
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-06-05 13:04:49 -0300

    Remove unused code and hard code

--- src/app/modules/private/components/user-profile/user-profile.component.ts
+++ src/app/modules/private/components/user-profile/user-profile.component.ts
@@ -1,35 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { RestaurantModel } from 'src/app/core/models/restaurant.model';
-import { LoginService } from 'src/app/core/services/login.service';
-import { RestaurantsService } from 'src/app/core/services/restaurants.service';
-import { ToastService } from 'src/app/core/services/toast.service';
 
 @Component({
   selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html'
+  templateUrl: './user-profile.component.html',
 })
 export class UserProfileComponent implements OnInit {
-  public restaurantsList: RestaurantModel[] = [];
+  constructor() {}
 
-  public slides: { src: string; alt: string }[] = [
-    {
-      src: 'assets/img/home/frete-gratis.svg',
-      alt: 'Frete gratis nas suas compras',
-    },
-    { src: 'assets/img/home/drones.svg', alt: 'Entrega por drones' },
-    { src: 'assets/img/home/coca.svg', alt: 'Propaganda coca-cola' },
-  ];
-
-  constructor(
-    private spinnerService: NgxSpinnerService,
-    private toastService: ToastService,
-    private restaurantsService: RestaurantsService,
-    private loginService: LoginService
-  ) {}
-
-  ngOnInit() {
-  }
-
-
+  ngOnInit() {}
 }
