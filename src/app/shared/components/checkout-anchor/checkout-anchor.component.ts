b271a08addecc690afc62b3dcdc7f81e867ceff0
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-06-15 11:19:03 -0300

    Create checkout anchor component

---
+++ src/app/shared/components/checkout-anchor/checkout-anchor.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-anchor',
+  templateUrl: './checkout-anchor.component.html',
+})
+export class CheckoutAnchorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
